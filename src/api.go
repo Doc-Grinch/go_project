3ca72f9ae0120043f85823947c9650f25947e9e8
Doc-Grinch <dtroyc@protonmail.com>
2022-10-19 16:21:45 +0200

    Adding comments in api.go

--- src/api.go
+++ src/api.go
@@ -17,8 +17,8 @@ type Park struct {
 
 var parks []Park
 
+// Function that returns the instance from the list of parks by searching by its name
 func parks_search_from_name(name string) (park *Park, index int) {
-	// return the instance from the list of parks by searching by its name
 	if len(parks) == 0 {
 		return nil, -1
 	}
@@ -30,12 +30,11 @@ func parks_search_from_name(name string) (park *Park, index int) {
 	return nil, -1
 }
 
+// Function that returns the instance from the list of parks by searching by its id
 func parks_search_from_id(id int64) (park *Park, index int) {
-	// return the instance from the list of parks by searching by its id
 	if len(parks) == 0 {
 		return nil, -1
 	}
-
 	for i, v := range parks {
 		if v.Id == id {
 			return &v, i
@@ -44,8 +43,8 @@ func parks_search_from_id(id int64) (park *Park, index int) {
 	return nil, -1
 }
 
+// Function that finds the largest id and return this id+1
 func parks_get_next_id() int64 {
-	// find the largest id and return this id+1
 	var res int64 = 0
 
 	var tmp int64
@@ -59,14 +58,14 @@ func parks_get_next_id() int64 {
 	return res + 1
 }
 
+// Function that removes element by combining two slices made of all elements to the one we remove and
+// Another slice made of all elements starting from the one we remove to the end of the original slice
 func parks_remove_from_index(index uint) error {
-	// remove element by combining two slices made of all elements to the one we remove and
-	// another slice made of all elements starting from the one we remove to the end of the original slice
 	if index > uint(len(parks)) {
 		errors.New("Out of range index")
 	}
 	if index == uint(len(parks))-1 {
-		// remove last element of the slice
+		// Remove last element of the slice
 		parks = append(parks[:index])
 	} else {
 		parks = append(parks[:index], parks[index+1:]...)
@@ -75,29 +74,32 @@ func parks_remove_from_index(index uint) error {
 	return nil
 }
 
+// Function to handle bad request
 func send_bad_req(reason string, w http.ResponseWriter) {
 	log.Print("Bad request from user: " + reason)
 	w.WriteHeader(http.StatusBadRequest)
-	// return empty response
+	// Return empty response
 	w.Write([]byte("{\"error\": \"" + reason + "\"}"))
 }
 
+// Function that handle not found pages
 func send_not_found(w http.ResponseWriter) {
-	// send a 404
+	// Send a 404
 	w.WriteHeader(http.StatusNotFound)
-	// return empty response
+	// Return empty response
 	w.Write([]byte("{}"))
 }
 
+// Function to handle good requests
 func send_ok(w http.ResponseWriter) {
-	// just send a 200 OK
+	// Just send a 200 OK
 	w.WriteHeader(http.StatusOK)
-	// return empty response
+	// Return empty response
 	w.Write([]byte("{}"))
 }
 
+// Function to send a park instance to the client
 func send_park(park *Park, w http.ResponseWriter) {
-	// send a park instance to the client
 	w.WriteHeader(http.StatusFound)
 	json_encoder := json.NewEncoder(w)
 	json_encoder.Encode(park)
@@ -105,15 +107,12 @@ func send_park(park *Park, w http.ResponseWriter) {
 
 func parse_request_json(w http.ResponseWriter, r *http.Request) (*Park, error) {
 	var search Park
-	// set default id to an invalid one
+	// Set default id to an invalid one
 	search.Id = -1
-
-	// decode json body
+	// Decode json body
 	json_decoder := json.NewDecoder(r.Body)
 	json_decoder.DisallowUnknownFields()
-
 	err := json_decoder.Decode(&search)
-
 	if err != nil {
 		// could not understand the request's body
 		return nil, err
@@ -122,6 +121,7 @@ func parse_request_json(w http.ResponseWriter, r *http.Request) (*Park, error) {
 	return &search, nil
 }
 
+// Function to handle the GET requests
 func handle_get(w http.ResponseWriter, r *http.Request) {
 	log.Print("Got a GET request from ", r.RemoteAddr)
 
@@ -131,14 +131,13 @@ func handle_get(w http.ResponseWriter, r *http.Request) {
 		send_bad_req("Got error when parsing JSON", w)
 		return
 	}
-
 	if search.Id < -1 {
-		// invalid id to search
+		// Invalid id to search
 		send_bad_req("'id' cannot be negative", w)
 		return
 	}
 	if search.Id == -1 && search.InPark == "" && search.Manufacturer == "" && search.Name == "" {
-		// send all parks if nothing was specified
+		// Send all parks if nothing was specified
 		w.WriteHeader(http.StatusFound)
 		value, _ := json.Marshal(parks)
 		w.Write(value)
@@ -147,10 +146,10 @@ func handle_get(w http.ResponseWriter, r *http.Request) {
 
 	var res *Park
 	if search.Id != -1 && search.Name == "" {
-		// seach by Id
+		// Seach by Id
 		res, _ = parks_search_from_id(search.Id)
 	} else if search.Id == -1 && search.Name != "" {
-		// seach by Name
+		// Seach by Name
 		res, _ = parks_search_from_name(search.Name)
 	} else {
 		send_bad_req("Cannot filter with 'id' and 'name' at the same time", w)
@@ -163,8 +162,9 @@ func handle_get(w http.ResponseWriter, r *http.Request) {
 	send_park(res, w)
 }
 
+// Function to handle the POST requests
 func handle_post(w http.ResponseWriter, r *http.Request) {
-	// here POST will update or create the park if it is not in the list
+	// Here POST will update or create the park if it is not in the list
 	log.Print("Got a POST request from ", r.RemoteAddr)
 
 	search, err := parse_request_json(w, r)
@@ -173,27 +173,24 @@ func handle_post(w http.ResponseWriter, r *http.Request) {
 		send_bad_req("Got error when parsing JSON", w)
 		return
 	}
-
 	if search.Id < -1 {
-		// invalid id to search
+		// Invalid id to search
 		send_bad_req("'id' cannot be negative", w)
 		return
 	}
-
 	if search.Id == -1 {
-		// tried to update a park without its id, should have made a PUT request instead
+		// Tried to update a park without its id, should have made a PUT request instead
 		send_bad_req("You need to specify 'id'", w)
 		return
 	}
-
 	_, index := parks_search_from_id(search.Id)
 
 	if index == -1 {
-		// value does not exist
+		// Value does not exist
 		send_not_found(w)
 	} else {
-		// value exists, updating it, note that some parts might have bee ommited in the input,
-		// so we are updating only things that was specified
+		// Value exists, updating it, note that some parts might have bee ommited in the input,
+		// So we are updating only things that was specified
 		if search.Name != "" {
 			parks[index].Name = search.Name
 		}
@@ -207,8 +204,9 @@ func handle_post(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+// Function to handle the DELETE requests
 func handle_delete(w http.ResponseWriter, r *http.Request) {
-	// delete a park from its id
+	// Delete a park from its id
 	log.Print("Got a DELETE request from ", r.RemoteAddr)
 
 	search, err := parse_request_json(w, r)
@@ -217,55 +215,52 @@ func handle_delete(w http.ResponseWriter, r *http.Request) {
 		send_bad_req("Got error when parsing JSON", w)
 		return
 	}
-
 	if search.Id <= -1 {
-		// invalid id to search
+		// Invalid id to search
 		send_bad_req("'id' cannot be negative", w)
 		return
 	}
-
 	var index int
 	_, index = parks_search_from_id(search.Id)
 
 	if index == -1 {
-		// value not found
+		// Value not found
 		send_not_found(w)
 		return
 	}
 
 	err = parks_remove_from_index(uint(index))
 	if err != nil {
-		// an error occured while removing
+		// An error occured while removing
 		send_not_found(w)
 		return
 	}
 	send_ok(w)
 }
 
+// Function to handle the PUT requests
 func handle_put(w http.ResponseWriter, r *http.Request) {
-	// create a new park
+	// Create a new park
 	log.Print("Got a PUT request from ", r.RemoteAddr)
-
 	search, err := parse_request_json(w, r)
-
 	if err != nil {
 		send_bad_req("Got error when parsing JSON", w)
 		return
 	}
-
 	if search.Name == "" || search.InPark == "" || search.Manufacturer == "" {
-		// if missing some elements
+		// If missing some elements
 		send_bad_req("'name', 'inPark' and 'manufacturer' have to be set when PUTing a new element.", w)
 		return
 	}
-	// generate a new id and append to the list of parks
+	// Generate a new id and append to the list of parks
 	search.Id = parks_get_next_id()
 	parks = append(parks, *search)
 	send_park(search, w)
 }
 
+// Function to handle all requests
 func handle_requests(w http.ResponseWriter, r *http.Request) {
-	// this function will call the corresponding function for the http method used
+	// This function will call the corresponding function for the http method used
 	switch r.Method {
 	case http.MethodGet:
 		handle_get(w, r)
@@ -276,16 +271,16 @@ func handle_requests(w http.ResponseWriter, r *http.Request) {
 	case http.MethodPut:
 		handle_put(w, r)
 	default:
-		// other http method than RESTful one
+		// Other http method than RESTful one
 		w.WriteHeader(http.StatusBadRequest)
 		w.Write([]byte(http.StatusText(http.StatusBadRequest)))
-		// send empty response
+		// Send empty response
 		w.Write([]byte("{}"))
 	}
 }
 
 func main() {
-	// intialize list of parks
+	// Intialize list of parks
 	parks = make([]Park, 0, 5)
 	fmt.Println("Welcome to your Rest API\nTo add a new data, you can try : curl -X PUT 127.0.0.1:8080/endpoint --data '{\"name\":\"Labyrinthe\", \"inPark\":\"Paris\", \"manufacturer\":\"Vortex\"}'")
 	fmt.Println("To retrieve a data : curl -X GET 127.0.0.1:8080/endpoint --data '{\"id\":1}'; you can get with id or name ")
